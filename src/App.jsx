c61145b95a21c79f78793d494737689434cccfcf
Om Mahajna <omrmahajan9477@gmail.com>
2025-12-08 10:45:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-neutral-900 flex items-center justify-center text-white">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
